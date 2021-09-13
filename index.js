0b62010f2099e1d15332b9d3639c42ec74c43350
0xngmi <0xngmi@protonmail.com>
2021-09-13 02:40:43 +0100

    change import

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const sushi = require('./DefiLlama-Adapters/projects/sushiswap/index')
+const sushi = require('./DefiLlama-Adapters/projects/sushiswap/api')
 const PORT = process.env.PORT || 5000
 
 let chainData = {}
