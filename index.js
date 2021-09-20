8dbd286e53a0cbc4dec29b03230dc168fedba9e4
0xngmi <0xngmi@protonmail.com>
2021-09-20 18:08:11 +0100

    add more projects

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
 const express = require('express')
 const sushi = require('./DefiLlama-Adapters/projects/sushiswap/api')
+const mdex = require('./DefiLlama-Adapters/projects/mdex/subgraphs')
+const synthetix = require('./DefiLlama-Adapters/projects/synthetix/index')
 const PORT = process.env.PORT || 5000
+const HOUR = 3600 * 1e3
 
 let chainData = {}
-const chains = Object.entries(sushi).filter(c=>c[1]?.tvl !== undefined).map(c=>c[0])
+const projects = {
+  sushi,
+  mdex,
+  synthetix
+}
 const retries = 5;
 
 express()
   .get('/', (req, res) => {
-    if(chains.every(c=>chainData[c]!== undefined)){
-      res.send(chainData)
-    } else {
-      throw new Error("Not all data available")
-    }
+    res.send(chainData)
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-setInterval(getData, 3600 * 1e3)
-
-function getData(){
-  console.log("starting")
-  chains.forEach(async chain=>{
-    for(let i=0; i<retries; i++){
-      const timestamp = Math.round(Date.now()/1e3)
-      try{
-        const balances = await sushi[chain].tvl(timestamp, undefined, {})
-        console.log(chain, timestamp)
-        chainData[chain] = balances
-        return;
-      } catch(e){
-        console.log(e)
-      }
-    }
-  })
+function clearData() {
+  chainData = Object.keys(projects).reduce((data, project) => {
+    data[project] = {}
+    return data
+  }, {})
 }
 
+setInterval(clearData, 12 * HOUR)
+clearData()
+setInterval(getData, HOUR)
 getData()
+
+function time(){
+  return Math.round(Date.now() / 1e3);
+}
+
+async function updateData(tvlFunction, project, chain) {
+  const start = time()
+  for (let i = 0; i < retries; i++) {
+    const timestamp = time()
+    if ((timestamp - start) > 45 * 60) {
+      return
+    }
+    try {
+      const balances = await tvlFunction(timestamp, undefined, {})
+      console.log(project, chain, timestamp)
+      chainData[project][chain] = balances
+      return;
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+function getData() {
+  console.log("starting")
+  Object.entries(projects).forEach(([name, project])=>{
+    const chains = Object.entries(project).filter(c => c[1]?.tvl !== undefined).map(c => c[0])
+    chains.forEach(chain => {
+      updateData(project[chain].tvl, name, chain)
+    })
+  })
+}
