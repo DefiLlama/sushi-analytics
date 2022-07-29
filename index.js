04e1ec54330b433251be0f5e7a4ee53eca47c4e0
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-07-29 18:01:01 +0200

    cosolidate timers

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const adaptersDir = './DefiLlama-Adapters/projects'
 
 let chainData = {}
 const projects = {
+  'harvest': require(adaptersDir + '/harvest.js'),
+  'hydradex': require(adaptersDir + '/hydradex.js'),
   'taiga': require(adaptersDir + '/taiga/api'),
   'tapio': require(adaptersDir + '/tapio/api'),
-  'karura-dex': require(adaptersDir + '/karura-dex/api'),
   'karura-lending': require(adaptersDir + '/karura-lending/api'),
   'karura-staking': require(adaptersDir + '/karura-staking/api'),
-  'acala-dex': require(adaptersDir + '/acala-dex/api'),
   'acala-lending': require(adaptersDir + '/acala-lending/api'),
   'acala-staking': require(adaptersDir + '/acala-staking/api'),
   'acala-lcdot': require(adaptersDir + '/acala-lcdot/api'),
   bifrost: require(adaptersDir + '/bifrost/api'),
   genshiro: require(adaptersDir + '/genshiro/api'),
+  'acala-dex': require(adaptersDir + '/acala-dex/api'),
+  'karura-dex': require(adaptersDir + '/karura-dex/api'),
 }
 
 const bulkyAdapters = {
@@ -26,6 +28,7 @@ const bulkyAdapters = {
   pinksale: require(adaptersDir + '/pinksale/index'),
   synthetix: require(adaptersDir + '/synthetix/api'),
   'team-finance': require(adaptersDir + '/team-finance/index'),
+  'xdao': require(adaptersDir + '/xdao.js'),
 }
 
 const retries = 2;
@@ -37,7 +40,7 @@ express()
   .get('/', (req, res) => {
     if (req.query.project) {
       // if a project and chain name is passed in query, just send over those info instead of all chain data
-      const {project, chain } = req.query
+      const { project, chain } = req.query
       let data = chainData[project]
       if (chain && data)
         data = { [chain]: data[chain] }
@@ -51,13 +54,10 @@ function clearData() {
   chainData = [...Object.keys(projects), ...Object.keys(bulkyAdapters)].reduce((acc, i) => ({ ...acc, [i]: {} }), {}) // reset chain data
 }
 
-setInterval(clearData, 12 * HOUR)
-setInterval(getData, HOUR)
-setInterval(updateBulkyData, HOUR)
+var i = 0
 
-clearData()
+setInterval(getData, HOUR)
 getData()
-updateBulkyData()
 
 function time() {
   return Math.round(Date.now() / 1e3);
@@ -75,12 +75,12 @@ async function updateData(tvlFunction, project, chain, onlyIfMissing = false) {
     }
     try {
       const balances = await tvlFunction(timestamp, undefined, {})
-      console.log(project, chain, timestamp)
+      console.log(i, project, chain, timestamp)
       if (!chainData[project]) chainData[project] = {}
       chainData[project][chain] = balances
       return;
     } catch (e) {
-      console.log(e)
+      console.error(e)
       // await sleepXMinutes(5)
     }
   }
@@ -91,15 +91,15 @@ async function sleepXMinutes(minutes = 10) {
 }
 
 async function getData() {
+  i++
+  if (i === 1200000) i = 0
+  if (i % 12) clearData()
   for (const [name, project] of Object.entries(projects)) {
     const chains = Object.entries(project).filter(c => c[1]?.tvl !== undefined).map(c => c[0])
     for (const chain of chains)
       await updateData(project[chain].tvl, name, chain)
   }
-}
 
-// Adapters are run in sequence here, we wait for running one to finish before starting the next
-async function updateBulkyData() {
   for (const [name, project] of Object.entries(bulkyAdapters)) {
     const chains = Object.entries(project).filter(c => c[1]?.tvl !== undefined).map(c => c[0])
     for (const chain of chains)
