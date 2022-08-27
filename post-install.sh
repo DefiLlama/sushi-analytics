53c760159b79022fe215884f1059d9cec445b4ab
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-08-27 11:43:24 +0200

    auto-update @defillama/sdk

--- post-install.sh
+++ post-install.sh
@@ -3,6 +3,8 @@
 [ ! -d "DefiLlama-Adapters" ] && git clone https://github.com/DefiLlama/DefiLlama-Adapters
 
 cd DefiLlama-Adapters
+git stash
 git checkout main
 git pull
 npm i
+npm update @defillama/sdk
