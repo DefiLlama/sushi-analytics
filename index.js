3b2e9877984626355c5964be3ac154d8dc10d76d
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-11-04 18:47:25 +0100

    refactor code to parallelize adapter calls

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const HOUR = 3600 * 1e3
 const fs = require('fs')
 const { exec } = require('child_process')
 const dataFile = 'data.json'
+const updateLog = 'updateLog.json'
 
 const server = http.createServer(async (req, res) => {
   //response headers
@@ -18,94 +19,17 @@ server.listen(PORT, () => {
   console.log(`server started on port: ${PORT}`);
 })
 
-const projectsGrouped = {
-  indie: {
-    'harvest': '/harvest.js',
-    'hydradex': '/hydradex.js',
-  // },
-  // bifrost: {
-    'bifrost-staking': '/bifrost-staking/api',
-    'bifrost-dex': '/bifrost-dex/api',
-  // },
-  // genshiro: {
-    'genshiro': '/genshiro/api',
-  // },
-  // indie: {
-    'sushiswap': '/sushiswap/api',
-  // },
-  // interlay: {
-    'interlay-btc': '/interlay-btc/api',
-    'interlay-staking': '/interlay-staking/api',
-    'interlay-collateral': '/interlay-collateral/api',
-    // },
-    // parallel: {
-    'parallel-staking': '/parallel-staking/api',
-    'parallel-crowdloan': '/parallel-crowdloan/api',
-    'parallelamm': '/parallelamm/api',
-    'parallel-lending': '/parallel-lending/api',
-    'parallel-stream': '/parallel-stream/api',
-  // },
-  // acala: {
-    'acala-staking': '/acala-staking/api',
-    'acala-lcdot': '/acala-lcdot/api',
-    'tapio': '/tapio/api',
-    // },
-    // karura: {
-    'karura-lending': '/karura-lending/api',
-    'karura-staking': '/karura-staking/api',
-    'taiga': '/taiga/api',
-    // },
-    // kintsugi: {
-    'kintsugi': '/kintsugi/api',
-  },
-  bulky: {
-    'kamino': '/kamino/api',
-    'xdao': '/xdao/apiCache',
-    unicrypt: '/unicrypt/apiCache',
-    dxsale: '/dxsale/apiCache',
-    dexpad: '/dexpad/apiCache',
-    'acala-lending': '/acala-lending/api',
-    'acala-dex': '/acala-dex/api',
-    'karura-dex': '/karura-dex/api',
-    deeplock: '/deeplock/apiCache',
-    pinksale: '/pinksale/apiCache',
-    'team-finance': '/team-finance/apiCache',
-    synthetix: '/synthetix/apiCache',
-  },
-}
+// initialize ata files
+fs.writeFileSync(dataFile, JSON.stringify({}))
+fs.writeFileSync(updateLog, JSON.stringify({}))
 
-function clearData() {
-  fs.writeFileSync(dataFile, JSON.stringify({})) // reset chain data
-}
+updateData()
+setInterval(updateData, HOUR)
 
-let i = 0
-
-clearData()
-getData()
-setInterval(getData, HOUR)
-
-async function getData() {
-  i++
-  if (i === 1200000) i = 0
-  if (i % 12) clearData()
-
-
-  for (const item of Object.entries(projectsGrouped)) {
-    await updateProjects(item)
-  }
-}
-
-async function updateProjects([group, projects]) {
-  for (const [name, project] of Object.entries(projects)) {
-    await updateProject(name, project, group === 'bulky')
-    console.log(new Date(), '[post-update]', name, project)
-  }
-}
-
-async function updateProject(name, project, onlyIfMissing) {
-  console.log(new Date(), '[pre-update]', name, project, onlyIfMissing)
-  return new Promise((resolve) => {
-    // ?? improve but batching here, so single connection object is made per polka chain
-    exec(['node', ' --max-old-space-size=1000', 'updateData.js', name, project, onlyIfMissing].join(' '), resolve)
+async function updateData(){
+  console.log(new Date(), '[pre-update all]')
+  await new Promise((resolve) => {
+    exec(['node', ' --max-old-space-size=1800', 'updateData.js'].join(' '), resolve)
   })
+  console.log(new Date(), '[post-update all]')
 }
--- updateData.js
+++ updateData.js
@@ -1,53 +1,53 @@
 const adaptersDir = './DefiLlama-Adapters/projects'
+const { bulky, hourlyRun } = require('./adapterMapping')
 const fs = require('fs')
 const dataFile = 'data.json'
 const logFile = 'debug.log'
 const errorFile = 'error.log'
+const updateLog = 'updateLog.json'
+const chainData = JSON.parse(fs.readFileSync(dataFile))
+const lastUpdateLog = JSON.parse(fs.readFileSync(updateLog))
+const HOUR = 3600 * 1e3
+const refreshFrequency = 12 * HOUR
 
-const retries = 4;
 function time() {
   return Math.round(Date.now() / 1e3);
 }
 
 function writeToFile(chain, project, data) {
-  const chainData = JSON.parse(fs.readFileSync(dataFile))
-  if (!chainData[project]) chainData[project] = {}
-  chainData[project][chain] = data
+  if (project) {
+    if (!chainData[project]) chainData[project] = {}
+    chainData[project][chain] = data
+  }
   fs.writeFileSync(dataFile, JSON.stringify(chainData))
+  fs.writeFileSync(updateLog, JSON.stringify(lastUpdateLog))
 }
 
 async function updateData(tvlFunction, project, chain, onlyIfMissing = false) {
-  const chainData = JSON.parse(fs.readFileSync(dataFile))
   if (onlyIfMissing) {
     if (chainData[project] && Object.keys(chainData[project][chain] || {}).length) return;  // update cache only if data is missing
   }
-  const start = time()
-  for (let i = 0; i < retries; i++) {
-    const timestamp = time()
-    if ((timestamp - start) > 45 * 60) {
-      return
-    }
-    try {
-      log('start', i, project, chain)
-      const balances = await tvlFunction(timestamp, undefined, {})
-      writeToFile(chain, project, balances)
-      log('done', i, project, chain, timestamp)
-      return;
-    } catch (e) {
-      error(project, chain, JSON.stringify(e))
-    }
+  const timestamp = time()
+  try {
+    log('[start]',project, chain)
+    const balances = await tvlFunction(timestamp, undefined, {})
+    writeToFile(chain, project, balances)
+    log('[done]',project, chain, 'time taken: ', time() - timestamp)
+  } catch (e) {
   }
 }
 
-function log() {
+function log(...args) {
+  console.log(...args)
   fs.appendFileSync(logFile, '\n')
-  fs.appendFileSync(logFile, [getDate(), ...arguments].join(' '))
+  fs.appendFileSync(logFile, [getDate(), ...args].join(' '))
 }
 
 
-function error() {
+function error(...args) {
+  console.log('[error]', ...args)
   fs.appendFileSync(errorFile, '\n')
-  fs.appendFileSync(errorFile, [getDate(), ...arguments].join(' '))
+  fs.appendFileSync(errorFile, [getDate(), ...args].join(' '))
 }
 
 function getDate() {
@@ -55,16 +55,47 @@ function getDate() {
 }
 
 async function updateProject(name, project, onlyIfMissing) {
-  project = require(adaptersDir + project)
-  const chains = Object.entries(project).filter(c => c[1]?.tvl !== undefined).map(c => c[0])
-  for (const chain of chains) {
-    for (const exportKey of Object.keys(project[chain])) {
-      const projectName = exportKey === 'tvl' ? name : `${name}-${exportKey}`
-      await updateData(project[chain][exportKey], projectName, chain, onlyIfMissing)
+  try {
+
+    const timeSinceLastUpdate = Date.now() - (lastUpdateLog[name] || 0)
+    if (timeSinceLastUpdate > refreshFrequency || !onlyIfMissing) {
+      delete chainData[project]
+      writeToFile()
     }
+
+    project = require(adaptersDir + project)
+
+    const chains = Object.entries(project).filter(c => c[1]?.tvl !== undefined).map(c => c[0])
+    for (const chain of chains) {
+      for (const exportKey of Object.keys(project[chain])) {
+        const projectName = exportKey === 'tvl' ? name : `${name}-${exportKey}`
+        await updateData(project[chain][exportKey], projectName, chain, onlyIfMissing)
+      }
+    }
+
+    lastUpdateLog[name] = Date.now()
+    writeToFile()
+
+  } catch (e) {
+    error(project, JSON.stringify(e))
+    delete chainData[project]
   }
-  process.exit(0)
 }
 
-const [_, _1, name, project, onlyIfMissing] = process.argv
-updateProject(name, project, onlyIfMissing === 'true')
+async function updateProjectGroup(group, onlyIfMissing) {
+  for (const [name, project] of Object.entries(group))
+    await updateProject(name, project, onlyIfMissing)
+}
+
+async function main() {
+  for (const projectGroups of hourlyRun)
+    await Promise.all(projectGroups.map(updateProjectGroup))
+  
+  return updateProjectGroup(bulky, true)
+}
+
+main().then(() => {
+  console.log('Done, exiting...')
+  writeToFile()
+  process.exit(0)
+})
