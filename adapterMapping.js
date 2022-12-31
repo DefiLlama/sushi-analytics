1e1db595ceacb8bd78225500c8a4e9eac11e9adf
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2022-12-31 23:09:39 +0100

    revert: run adapter one at a time

--- adapterMapping.js
+++ adapterMapping.js
@@ -8,40 +8,56 @@ module.exports = {
         '10kswap': '/10kswap/api',
         'myswap': '/myswap/api',
         'injective-orderbook': '/injective-orderbook/api',
+      },
+      {
         'bifrost-staking': '/bifrost-staking/api',
         'bifrost-dex': '/bifrost-dex/api',
         'bifrost-liquid-crowdloan': '/bifrost-liquid-crowdloan/api',
+      },
+      {
         'genshiro': '/genshiro/api',
+      },
+      {
         'interlay-btc': '/interlay-btc/api',
         'interlay-staking': '/interlay-staking/api',
         'interlay-collateral': '/interlay-collateral/api',
+      },
+    ],
+    [
+      {
         'parallel-staking': '/parallel-staking/api',
         'parallel-crowdloan': '/parallel-crowdloan/api',
         'parallelamm': '/parallelamm/api',
         'parallel-lending': '/parallel-lending/api',
         'parallel-stream': '/parallel-stream/api',
+      },
+      {
         'acala-staking': '/acala-staking/api',
         'acala-lcdot': '/acala-lcdot/api',
         'tapio': '/tapio/api',
         'acala-lending': '/acala-lending/api',
         'acala-dex': '/acala-dex/api',
+      },
+      {
         'karura-lending': '/karura-lending/api',
         'karura-staking': '/karura-staking/api',
         'taiga': '/taiga/api',
         'karura-dex': '/karura-dex/api',
+      },
+      {
         'kintsugi': '/kintsugi/api',
       },
     ],
   ],
   bulky: {
     '1inch': '/1inch/apiCache',
+    dxsale: '/dxsale/apiCache',
+    'dogeswap-org': '/dogeswap-org/api',
     unicrypt: '/unicrypt/apiCache',
     deeplock: '/deeplock/apiCache',
     pinksale: '/pinksale/apiCache',
     'team-finance': '/team-finance/apiCache',
     synthetix: '/synthetix/apiCache',
-    dxsale: '/dxsale/apiCache',
-    'dogeswap-org': '/dogeswap-org/api',
     'kamino': '/kamino/api',
   },
 }
