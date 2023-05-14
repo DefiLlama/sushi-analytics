192ba1a8048f6c8e502f4798e5668fc012984e50
gintoki <llama@llama.fi>
2023-05-14 14:13:53 +0200

    update npm packages

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,9 @@
 npm update  @defillama/sdk @hubbleprotocol/kamino-sdk
 
+git config --global user.email llama@llama.fi
+git config --global user.name llama
+git stash
+
 [ ! -d "DefiLlama-Adapters" ] && git clone https://github.com/DefiLlama/DefiLlama-Adapters
 
 cd DefiLlama-Adapters
