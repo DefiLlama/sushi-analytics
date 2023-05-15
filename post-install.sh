615de82b1f2418589a1b6d711335829a059316df
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-15 11:20:27 +0200

    Revert "update npm packages"
    This reverts commit 192ba1a8048f6c8e502f4798e5668fc012984e50.

--- post-install.sh
+++ post-install.sh
@@ -1,9 +1,5 @@
 npm update  @defillama/sdk @hubbleprotocol/kamino-sdk
 
-git config --global user.email llama@llama.fi
-git config --global user.name llama
-git stash
-
 [ ! -d "DefiLlama-Adapters" ] && git clone https://github.com/DefiLlama/DefiLlama-Adapters
 
 cd DefiLlama-Adapters
