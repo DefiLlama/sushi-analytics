32164d80cb0a9a2e6cf638ba8d802cbcae029f15
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-05-15 11:25:40 +0200

    stash changes introduced

--- post-install.sh
+++ post-install.sh
@@ -1,5 +1,9 @@
 npm update  @defillama/sdk @hubbleprotocol/kamino-sdk
 
+git config --global user.email llama@llama.fi
+git config --global user.name llama
+git stash
+
 [ ! -d "DefiLlama-Adapters" ] && git clone https://github.com/DefiLlama/DefiLlama-Adapters
 
 cd DefiLlama-Adapters
@@ -8,3 +12,4 @@ git checkout main
 git pull
 npm i
 npm update @defillama/sdk
+git stash
