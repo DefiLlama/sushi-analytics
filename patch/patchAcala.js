735e56de785bbe3ed29dedb1125c2a7cd5ee8efc
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2023-11-15 09:10:00 +0100

    patch acala npm module

--- patch/patchAcala.js
+++ patch/patchAcala.js
@@ -1,7 +1,14 @@
 const fs = require('fs');
 
-const sourcePath = __dirname + '/storage.js';
-const destinationPath = __dirname + '/../node_modules/@acala-network/sdk/utils/storage/storage.js';
+// const sourcePath = __dirname + '/storage.js';
+// const destinationPath = __dirname + '/../node_modules/@acala-network/sdk/utils/storage/storage.js';
+
+const sourcePath = __dirname + '/../node_modules/@acala-network/eth-providers/lib/consts.js';
+const destinationDir = __dirname + '/../node_modules/@acala-network/eth-providers/lib/lib';
+const destinationPath = destinationDir + '/consts.js';
+
+if (!fs.existsSync(destinationDir))
+  fs.mkdirSync(destinationDir);
 
 // Copy the file
 fs.copyFile(sourcePath, destinationPath, (err) => {
