6406a253781e35ac19576e4bd9434cbfe65ae05f
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2024-11-05 12:12:10 +0100

    kamino: update sdk

--- post-install.sh
+++ post-install.sh
@@ -1,15 +1,16 @@
 npm update  @defillama/sdk @hubbleprotocol/kamino-sdk
 
-git config --global user.email llama@llama.fi
-git config --global user.name llama
-git stash
+# git config --global user.email llama@llama.fi
+# git config --global user.name llama
+# git stash
 
+# 
 [ ! -d "DefiLlama-Adapters" ] && git clone https://github.com/DefiLlama/DefiLlama-Adapters
 
-cd DefiLlama-Adapters
-git stash
-git checkout main
-git pull
-npm i
-npm update @defillama/sdk
-git stash
+# cd DefiLlama-Adapters
+# git stash
+# git checkout main
+# git pull
+# npm i
+# npm update @defillama/sdk
+# git stash
